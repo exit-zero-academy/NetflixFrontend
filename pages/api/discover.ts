a473c26ab24212032324518eb43ba0f1dd7e9604
alonit <alon.itach@mail.huji.ac.il>
2025-02-19 21:41:47 +0200

    upgrade node version support

--- pages/api/discover.ts
+++ pages/api/discover.ts
@@ -24,7 +24,8 @@ export default async function handler(request: NextApiRequest, response: NextApi
 
     response.status(200).json({ type: 'Success', data });
   } catch (error) {
-    console.log(error.data);
-    response.status(500).json({ type: 'Error', data: error.data });
+    const err = error as { data?: any }; // Type assertion
+    console.log(err.data);
+    response.status(500).json({ type: 'Error', data: err.data });
   }
 }
--- pages/search.tsx
+++ pages/search.tsx
@@ -5,7 +5,8 @@ const Layout = dynamic(import('../components/Layout'));
 export default function Search() {
     return (
         <Layout>
-            
+            <h1>Search Page</h1>
+            <p>Welcome to the search page!</p>
         </Layout>
     )
 }
